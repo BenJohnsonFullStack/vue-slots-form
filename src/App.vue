b992bbc13da523c95c62013ec0075162ef4da34c
Ben Johnson <monk.sound.design@gmail.com>
2023-12-08 21:36:11 -0500

    named slots

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <Form>
-    <div class="help">
-      <p>This is some text</p>
-    </div>
-    <div class="fields">
+    <template v-slot:help>
+      <p>This is some help text</p>
+    </template>
+    <template v-slot:fields>
       <input type="text" placeholder="email" />
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
-    </div>
-    <div class="buttons">
+    </template>
+    <template v-slot:buttons>
       <button type="submit">Submit</button>
-    </div>
+    </template>
   </Form>
 
-  <Form></Form>
+  <Form>
+    <template v-slot:help>
+      <p>Contact help text.</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="name" />
+      <input type="text" placeholder="message" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </Form>
 </template>
 
 <script>
