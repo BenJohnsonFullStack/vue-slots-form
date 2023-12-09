dc0d18df361f019acea2fc6aa0462dcadf7af8cb
Ben Johnson <monk.sound.design@gmail.com>
2023-12-08 21:23:09 -0500

    form component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
-  <form>
-    <div class="help">
-      <p>This is some text</p>
-    </div>
-    <div class="fields">
-      <input type="text" placeholder="email" />
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-    </div>
-    <div class="buttons">
-      <button type="submit">Submit</button>
-    </div>
-  </form>
+  <Form />
 </template>
 
 <script>
+import Form from "@/components/Form.vue";
+
 export default {
   name: "App",
+  components: { Form },
 };
 </script>
