4f1ac5eb724a3ba5a30b9690796c45061dd9ac15
Ben Johnson <monk.sound.design@gmail.com>
2023-12-08 21:30:20 -0500

    slots

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <Form />
+  <Form>
+    <div class="help">
+      <p>This is some text</p>
+    </div>
+    <div class="fields">
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+    </div>
+    <div class="buttons">
+      <button type="submit">Submit</button>
+    </div>
+  </Form>
+
+  <Form></Form>
 </template>
 
 <script>
